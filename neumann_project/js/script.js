180bbbbeb3df700ce3963ccb3731e3ce50603f97
PAndras <90793913+PazmanAndras@users.noreply.github.com>
2023-10-08 10:17:53 +0200

    --

---
+++ neumann_project/js/script.js
@@ -0,0 +1,7 @@
+// Example JavaScript code
+// You can add more functionality as needed
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add your JavaScript code here
+    console.log("Website is loaded!");
+});
